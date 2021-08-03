724582addbea6a1f8c854737c9a63a204260742c
BrentonPhoenix <brentonphoenix@gmail.com>
2021-08-03 13:12:49 -0400

    first add

---
+++ research/Randomizer.js
@@ -0,0 +1,40 @@
+let random = Math.random()
+//a method to get a random number
+//console.log(random)
+
+
+//number between one and ten
+
+let random10 = Math.floor(Math.random() * 10) + 1;
+
+//console.log(random10)
+
+
+//lets make a randomizer for between one and onehundred!
+
+let random100 = Math.floor(Math.random() * 100) + 1
+let random100Two = Math.floor(Math.random() * 100) + 1
+
+//console.log(random100)
+
+//function randomMath(x , y){
+//    console.log(x, y)
+//    console.log(x + y)
+//}
+
+randomMath(random100, random100Two)//created random100Two   changed second variable from random100 to random100Two
+//randomMath(random100, random100) results in random 100 having the same value since it only runs once need random100Two for the second value
+
+let testNoPlusOne = Math.floor(Math.random() *10) 
+let testNoPlusOne2 = Math.floor(Math.random() *10) 
+let testNoPlusOne3 = Math.floor(Math.random() *10) 
+let testNoPlusOne4 = Math.floor(Math.random() *10) 
+let testNoPlusOne5 = Math.floor(Math.random() *10) 
+let testNoPlusOne6 = Math.floor(Math.random() *10) 
+
+console.log(testNoPlusOne)
+console.log(testNoPlusOne2)
+console.log(testNoPlusOne3)
+console.log(testNoPlusOne4)
+console.log(testNoPlusOne5)
+console.log(testNoPlusOne6)
